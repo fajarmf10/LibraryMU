18e42f683b64cf06f5204bd9387086eff5cfd24c
sosispanggang <sosispanggang.15@gmail.com>
2018-05-16 12:39:15 +0700

    Some shit

---
+++ books.php
@@ -0,0 +1,124 @@
+<?php
+  session_start();
+  if(!isset($_SESSION['username'])){
+    header("Location: login.php");
+    die;
+  }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>LibraryMU - Books Collection</title>
+  <?php include('meta.php'); ?>
+  <style type="text/css">
+    .pie_progress {
+      width: 160px;
+      margin: 10px auto;
+    }
+
+    @media all and (max-width: 768px) {
+      .pie_progress {
+        width: 80%;
+        max-width: 300px;
+      }
+    }
+
+    pre{
+      height: 250px;
+      overflow: scroll;
+    }
+
+    .title{
+      height: 50px;
+    }
+  </style>
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <?php
+  include('navbar.php');
+  include('sidebar.php');
+  ?>
+
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>LibraryMU's Books Collection</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="/">Home</a></li>
+              <li class="breadcrumb-item active">Books Collection</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Collections</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+              <table id="example2" class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Category</th>
+                  <th>Uploaded At</th>
+                  <th>Total Readers</th>
+                  <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>Trident</td>
+                  <td>Internet
+                    Explorer 4.0
+                  </td>
+                  <td>28 Oktober 2010</td>
+                  <td>Win 95+</td>
+                  <td>
+                    <div class="text-center">
+                      <a href="https://fajarmf.com/.pdf/advanced-machine-learning-with-python.pdf" target="_blank"><i class="fa fa-search-plus" title="Read Now" aria-hidden="true"></i></a>
+                      <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                      <i class="fa fa-plus-circle" title="Add to Library" aria-hidden="true"></i>
+                    </div>
+                  </td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+
+          </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+
+  </div>
+  <!-- /.content-wrapper -->
+
+  <?php include('footer.php'); ?>
+
+</div>
+<!-- ./wrapper -->
+
+<?php include('script.php'); ?>
+</body>
+</html>
